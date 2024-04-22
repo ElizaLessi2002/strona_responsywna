676b95b3e6e1a2301907430438dd6500cfd7de7b
Eliza Lesiewicz <eliza20122002@wp.pl>
2024-04-22 17:15:27 +0200

    Dodanie folderu backend

---
+++ backend/php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> <!--rozpoczecie kodu html -->
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8"/> <!-- pozwala zapisywanie bez bledow slow w polskim jezyku -->
+    <meta name="author" content="Eliza Lesiewicz"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/> 
+    <!-- pozwala na prawidlowe funkcjonowanie strony internetowej w przegladarce Edge -->
+</head>
+
+<body>
+
+    <?php //rozpoczecie kodu w php
+
+    echo "Hello World" //wyswietlenie tekstu
+
+    ?> <!-- zakonczenie kodu php -->
+
+</body>
+
+
+
+</html> <!-- zakonczenie kodu html -->
+
